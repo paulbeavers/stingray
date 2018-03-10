68a68cbebfc786b23f871a46d6bea9d307d40be6
Paul Beavers <pbeavers@silverado.localdomain>
2018-03-10 03:32:55 -0600

    Commit

--- restserver/index.js
+++ restserver/index.js
@@ -7,22 +7,33 @@ var server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.fullResponse());
 
-server.post("/documents", createDocument)
-server.get("/documents/:id", getDocumentById)
+controllers_path = process.cwd() + '/controllers'
 
-function createDocument(req, res, next)
-{
-	console.log("createDocument")
-	console.log(req.body)
-	res.json({type: true, response:"createDocument succcess"})
-}
 
-function getDocumentById(req, res, next)
-{
-	console.log("createDocument")
-	res.json({type: true, response:"createDocument succcess"})
-}
+/*-----------------------------------------------------------*/
+/* Load the controllers from the controllers directory       */
+/*-----------------------------------------------------------*/
 
+var controllers = {}
+
+controllers_path = process.cwd() + '/controllers'
+
+fs.readdirSync(controllers_path).forEach(function (file) {
+	if (file.indexOf('.js') != -1) {
+	    controllers[file.split('.')[0]] = require(controllers_path + '/' + file)
+	}
+})
+
+/*-----------------------------------------------------------*/
+/*  Set up the documents resource                            */
+/*-----------------------------------------------------------*/
+documentController = require(controllers_path + "/" + "documents.js")
+server.post("/documents", controllers.documents.createDocument)
+server.get("/documents/:id", controllers.documents.getDocumentById)
+
+/*---------------------------------------------------------------*/
+/*  Start the REST API server                                    */
+/*---------------------------------------------------------------*/
 var port = process.env.PORT || 3000;
 server.listen(port, function (err) {
 	if (err)
