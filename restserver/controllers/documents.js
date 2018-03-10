a50863f104a39da5ddb41932e38e7905477cd547
Paul Beavers <pbeavers@silverado.localdomain>
2018-03-10 03:31:48 -0600

    Setting up the controllers structure

---
+++ restserver/controllers/documents.js
@@ -0,0 +1,14 @@
+
+
+exports.createDocument = function(req, res, next) {
+
+	console.log("createDocument")
+	        console.log(req.body)
+	        res.json({type: true, response:"createDocument succcess"})
+
+}
+
+exports.getDocumentById = function(req, res, next){
+
+
+}
