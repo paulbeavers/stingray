52b8def1ceb8df5613b4ff2bbbd334790871767f
Paul Beavers <paul_beavers@mac.com>
2018-06-24 11:13:21 -0500

    added document example

---
+++ restserver/examples/adddocument.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#-------------------------------------------------------------
+# Example script to call restserver API
+# adminuser
+#-------------------------------------------------------------
+
+curl -H "Content-Type: application/json"  \
+	-X POST http://localhost:2999/document \
+	-u STINGRAY:stingraypw \
+-d  @- <<'EOF'
+
+{  "tenant_name":"New Tenant 2",  
+    "user_id":"paul_pbeavers@mac.com", 
+    "message_text":"goodbye cruel world", 
+    "device_name":"restserver.pbeavers.net"  
+}
+
+EOF
