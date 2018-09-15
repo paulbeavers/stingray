0a9e452390c9b927fba488d1f4ed65efe188b59c
root <paul_beavers@mac.com>
2018-09-15 18:40:59 +0000

    working on container config

--- setup/dockersetup.bash
+++ setup/dockersetup.bash
@@ -7,7 +7,7 @@ echo installing node.js
 apt-get install node.js
 apt-get install npm
 cd /opt/stingray/restserver
-sudo npm install restify
-sudo npm install pg
-sudo npm install winston
+npm install restify
+npm install pg
+npm install winston
 
--- setup/setup.bash
+++ setup/setup.bash
@@ -4,9 +4,9 @@ echo Installing prequisites
 
 echo installing node.js
 
-sudo apt-get install node.js
-sudo apt-get install npm
-cd ../restserver
+apt-get install node.js
+apt-get install npm
+cd /opt/stingray/restserver
 sudo npm install restify
 sudo npm install pg
 sudo npm install winston
