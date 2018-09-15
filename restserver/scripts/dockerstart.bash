e36b4f0c5bf13d0334bfbd310924e381900b17b1
root <paul_beavers@mac.com>
2018-09-15 19:11:52 +0000

    added dockerstart

---
+++ restserver/scripts/dockerstart.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------
+# startup script for restserver
+#---------------------------------------------------------------------
+
+#---------------------------------------------------------------------
+# Load configuration
+#---------------------------------------------------------------------
+export PORT=2999
+export SERVER_HOME=/opt/stingray/restserver
+export DATA_DIR="$SERVER_HOME/data"
+export DB_STRING=postgres://stingray_user:stingraypw@192.168.166.183:5432/stingraydb
+export DB_HOSTNAME=192.168.166.183
+export DB_PORT=5432
+export DB_USER=stingray_user
+export DB_PASSWORD=stingraypw
+export DB_DATABASE=stingraydb
+export LOG_DIR="$SERVER_HOME/log"
+export LOG_LEVEL="info"
+
+#---------------------------------------------------------------------
+# Start the server
+#---------------------------------------------------------------------
+cd ../app
+node ./index.js
+
+
